9ab83acf87f3d5e29fc58699d92e02dd46a5373e
Halcik <julkatablet2507@gmail.com>
2024-08-04 20:11:04 +0200

    displaying the status of a task

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# daily
-xyz
+# Daily
+Jest to kolejna aplikacja służąca do planowania. Łączy ze sobą elementy kalendarza, plannera oraz wybranych elementów bullet journala m.in. śledzenie nastroju, snu oraz pogody.
+>Projekt jest na etapie prototypowania.
+***
+
+## Funkcje występujące w programie:
+
+ - **add_task_to_file** - odbiera listę z danymi zadania i zapisuje je do pliku **tasks.txt**
+
+ - **new_task** - umożliwia tworzenie nowego zadania przez użytkownika. Zadanie jest przesyłane następnie do funkcji **add_task_to_file**
+
+ - **read_tasks** - odczytuje zadania i segreguje je według daty - zadania z aktualną datą są w zadaniach na dany dzień, natomiast te, których data minęła a nie zostały wykonane, znajdują się w zaległych. Przyszłe zadania nie są wyświetlane.
+
+ - **update_status** - pozwala na zaktualizowanie statusu zadania po jego id (uzyskiwane przy odczycie zadań przez **read_tasks**)
--- main.py
+++ main.py
@@ -15,49 +15,63 @@ def new_task():
     category = input("Podaj nazwę kategorii:\n")
     duration = input("Podaj, ile czasu trwa wykonanie zadania w min:\n")
     status = "NO"
+    end_date = "0 0 0"
     id_file = open("id.txt", "r", encoding='utf-8')
     id = str(int(id_file.readline())+1)
     id_file.close()
     id_file = open("id.txt", "w", encoding='utf-8')
-    task = [date, id, name, category, duration, status]
+    task = [date, id, name, category, duration, status, end_date]
     id_file.write(id)
     add_task_to_file(task)
 
 
-def read_tasks(get_date):
+def read_tasks(date_now):
     tasks = open("tasks.txt", "r", encoding='utf-8')
     tasks_tab = tasks.readlines()[1:]
     i = 1
     print("Zadania na dziś:")
     for task in tasks_tab:
-        date = task.split()[0:3]
+        task = task.split("  ")
+        date = task[0].split()
         date = datetime(day=int(date[0]), month=int(
             date[1]), year=int(date[2]))
-        if date == get_date:
-            print(i, end=". ")
-            print(task.replace("\n", "")[:-3])
+        if date == date_now:
+            if task[5]=="NO":
+                status = "☐"
+            else:
+                status = "☑"
+            print(f"{i}.{status} [id {task[1]}] {task[2]} - {task[4]} min.")
             i += 1
     print("\nZaległe:")
     for task in tasks_tab:
-        date = task.split()[0:3]
+        task = task.split("  ")
+        date = task[0].split()
         date = datetime(day=int(date[0]), month=int(
             date[1]), year=int(date[2]))
-        if date < get_date:
-            print(i, end=". ")
-            print(task.replace("\n", "")[:-3])
+        if date < date_now:
+            if task[5]=="NO":
+                status = "☐"
+            else:
+                status = "☑"
+            print(f"{i}.{status} [id {task[1]}] {task[2]} ({task[3]}) z {task[0].replace(' ', '.')} - {task[4]} min.")
             i += 1
     tasks.close()
 
 
-def update_status():
+def update_status(date_now):
+    counter_file = open("counter.txt", "r", encoding='utf-8')
+    counter = counter_file.readline().split("  ")
+    print(counter)
     id = input('Podaj id zadania: ')
     tasks = open("tasks.txt", "r", encoding='utf-8')
     tasks_tab = tasks.readlines()
     tasks.close()
+    counter_file.close()
     for task in tasks_tab:
         task = task.split("  ")
         if task[1] == id:
-            task[-1] = "YES\n"
+            task[-2] = "YES"
+            task[-1] = f"{date_now.day} {date_now.month} {date_now.year}\n"
             task = '  '.join(task)
             tasks_tab[int(id)] = task
             tasks = open("tasks.txt", "w", encoding='utf-8')
@@ -68,7 +82,6 @@ def update_status():
 if __name__ == '__main__':
     # task = new_task()
     date_now = datetime.now()
-    date_now = datetime(
-        day=date_now.day, month=date_now.month, year=date_now.year)
-    read_tasks(date_now)
-    update_status()
+    date_now = datetime(day=date_now.day, month=date_now.month, year=date_now.year)
+    #read_tasks(date_now)
+    update_status(date_now)
